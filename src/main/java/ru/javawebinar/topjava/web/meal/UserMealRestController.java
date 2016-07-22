208176e5d457f9e94c623b6e311e66bb02ee407d
testim <tgumerov@gmail.com>
2016-07-22 11:05:07 +0500

    patch 8_03_formatter

--- src/main/java/ru/javawebinar/topjava/web/meal/UserMealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/UserMealRestController.java
@@ -8,6 +8,9 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.to.UserMealWithExceed;
 import java.time.LocalDateTime;
+
+import java.time.LocalTime;
+import java.time.LocalDate;
 import java.util.List;
 
 import java.net.URI;
@@ -57,6 +60,13 @@ public class UserMealRestController extends AbstractUserMealController {
             @RequestParam(value = "startDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDateTime,
             @RequestParam(value = "endDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endDateTime) {
         return super.getBetween(startDateTime.toLocalDate(), startDateTime.toLocalTime(), endDateTime.toLocalDate(), endDateTime.toLocalTime());
+    }
+
+    @RequestMapping(value = "/filter", method = RequestMethod.GET)
+    public List<UserMealWithExceed> getBetween(
+            @RequestParam(value = "startDate", required = false) LocalDate startDate, @RequestParam(value = "startTime", required = false) LocalTime startTime,
+            @RequestParam(value = "endDate", required = false) LocalDate endDate, @RequestParam(value = "endTime", required = false) LocalTime endTime) {
+        return super.getBetween(startDate, startTime, endDate, endTime);
     }
 }
 
